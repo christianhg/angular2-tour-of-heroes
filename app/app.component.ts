3f1d82cbd59095b1dec6c9a8931af42d87748691
Christian Grøngaard <christianhg@gmail.com>
2016-03-25 14:02:04 +0100

    Fix identation

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,20 @@ import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
-    selector: 'my-app',
-    template: `
-      <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="#hero of heroes"
-          [class.selected]="hero === selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-      `,
-      styles:[`
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+  styles:[`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
