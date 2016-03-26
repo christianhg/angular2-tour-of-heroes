2cda92871e8dca1150077aa05126c410ac716bba
Christian Grøngaard <christianhg@gmail.com>
2016-03-26 13:03:39 +0100

    Move hero service and interface to separate dir to emphasize modularity

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroService} from './hero.service';
+import {HeroService} from './hero/hero.service';
 import {HeroesComponent} from './heroes/heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 
