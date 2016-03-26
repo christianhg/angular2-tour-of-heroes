fdc79aeeea7fc62a7f43b1efcec7e57027681de1
Christian Grøngaard <christianhg@gmail.com>
2016-03-26 12:55:12 +0100

    Move dashboard component to separate dir to emphasize modularity

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
-import {DashboardComponent} from './dashboard.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
