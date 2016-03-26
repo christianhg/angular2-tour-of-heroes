41f117a3ecf8d886d931c99b6bb2143540cdeecb
Christian Grøngaard <christianhg@gmail.com>
2016-03-26 12:56:36 +0100

    Move heroes component to separate dir to emphasize modularity

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroService} from './hero.service';
-import {HeroesComponent} from './heroes.component';
+import {HeroesComponent} from './heroes/heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
